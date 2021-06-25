63d3cc43038e9324673853ad807a6928b0184b55
Student <Student@DESKTOP-7BR0KVL>
2021-06-25 09:17:20 +0300

    Добавьте файлы проекта.

---
+++ SessiaBaryshev/Object.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessiaBaryshev
+{
+    class Object
+    {
+        public string nameObject;
+        public string ownerName;
+        public DateTime dateInLibrary; //year, month, day
+    }
+
+        class Book : Object
+        {
+            public string Author;
+            public string Illustrator;
+            public string Publisher;
+            public DateTime bornYear;
+        }
+
+        class Puzzle : Object
+        {
+            public int valueElements;
+            public string company;
+        }
+
+        class tableGame : Object
+        {
+            public string developer;
+            public string gameplay;
+            public string valuePeople;
+        }
+}
